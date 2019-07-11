97279d0e0179166aeccf551f2d18907f5476be49
Vojtech Bocek <vbocek@gmail.com>
2019-07-11 16:05:10 +0200

    Use nsis installer instead of portable

---
+++ win-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo docker run -ti  --env ELECTRON_CACHE="/root/.cache/electron" \
+    --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
+    -v ${PWD}:/project  -v ${PWD##*/}-node-modules:/project/node_modules  \
+    -v ~/.cache/electron:/root/.cache/electron  \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder  \
+    electronuserland/builder:wine
